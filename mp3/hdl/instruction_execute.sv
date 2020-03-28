b4e55a1fd50611bd144a21695fdb539d44d1c814
Louis Kim <Louis@Louiss-MacBook-Pro.local>
2020-03-28 01:46:34 -0500

    Finished execute and memory access stages

--- mp3/hdl/instruction_execute.sv
+++ mp3/hdl/instruction_execute.sv
@@ -18,10 +18,12 @@ module instruction_execute
   output rv32i_word PC_out,
   output rv32i_word alu_out,
   output rv32i_word rs2_out,
-  output br_en_out
+  output br_en_out,
+  output [3:0] mem_byte_enable_out
 );
 
 rv32i_word alu_o;
+logic [3:0] mem_byte_enable;
 logic br_en;
 
 alu alu (
@@ -35,6 +37,53 @@ cmp cmp (
   .*,
   .cmpop (ctrl_word_in.cmpop)
 );
+
+always_comb begin
+  //set byte enable
+  //note that rs2 (write data) must be masked using byte enable,
+  //done in mem_access stage to reduce logic in this stage
+  //note that sw has same encoding as lw, etc. so we can account for both cases
+  unique case (data[14:12])
+    default:
+      mem_byte_enable = 4'b1111;
+    load_funct3_t::lh:, load_funct3_t::lhu: begin
+      unique case (alu_o[1:0])
+        2'b00:
+          mem_byte_enable = 4'b0011;
+        2'b01:
+          mem_byte_enable = 4'b0110;
+        2'b10:
+          mem_byte_enable = 4'b1100;
+        2'b11:
+          mem_byte_enable = 4'b1000;
+      endcase
+    end
+    load_funct3_t::lw: begin
+      unique case (alu_o[1:0])
+        2'b00:
+          mem_byte_enable = 4'b1111;
+        2'b01:
+          mem_byte_enable = 4'b1110;
+        2'b10:
+          mem_byte_enable = 4'b1100;
+        2'b11:
+          mem_byte_enable = 4'b1000;
+      endcase
+    end
+    load_funct3_t::lb:, load_funct3_t::lbu: begin
+      unique case (alu_o[1:0])
+        2'b00:
+          mem_byte_enable = 4'b0001;
+        2'b01:
+          mem_byte_enable = 4'b0010;
+        2'b10:
+          mem_byte_enable = 4'b0100;
+        2'b11:
+          mem_byte_enable = 4'b1000;
+      endcase
+    end
+  endcase
+end
 
 always_ff @(negedge clk) begin
   if (rst) begin
@@ -52,6 +101,7 @@ always_ff @(negedge clk) begin
     alu_out <= alu_o;
     rs2_out <= rs2_o;
     br_en_out <= br_en;
+    mem_byte_enable_out <= mem_byte_enable;
   end
 end
 endmodule
--- mp3/hdl/memory_access.sv
+++ mp3/hdl/memory_access.sv
@@ -1,4 +1,42 @@
 module memory_access
 (
+input clk,
+input rst,
+input rv32i_word PC_in,
+input rv32i_word instruction_in,
+input rv32i_control_word ctrl_word_in,
+input [3:0] mem_byte_enable_in,
+input rv32i_word rs2_out,
+input rv32i_word alu_out,
+input rv32i_word data_rdata_in, //read word from memory
+input data_resp,
 
+output rv32i_control_word ctrl_word_out,
+output rv32i_word instruction_out,
+output logic [31:0] data_addr,
+output logic [31:0] data_wdata,
+output logic [3:0] data_mbe,
+output logic [3:0] mem_byte_enable_out,
+output logic [31:0] data_rdata, //output to next stage, not output to memory
 );
+
+assign data_addr = {alu_out[31:2], 2'b00};
+assign data_mbe = mem_byte_enable_in;
+assign data_wdata = rs2_out;
+
+//set state register outputs
+always_ff @(negedge clk) begin
+  if (rst) begin
+    ctrl_word_out <= 0;
+    instruction_out <= 0;
+    mem_byte_enable_out <= 0;
+    data_rdata <= 0;
+  end
+  else begin
+    ctrl_word_out <= ctrl_word_in;
+    instruction_out <= instruction_in;
+    mem_byte_enable_out <= mem_byte_enable_in;
+    //rdata holds its value until resp goes high from memory, but for now we will always get once cycle hit
+    data_rdata <= data_resp ? data_rdata_in : data_rdata;
+  end
+end
