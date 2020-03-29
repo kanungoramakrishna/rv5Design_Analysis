ea7b065d03079148e47b98cb005f68a25f58f104
Louis <ltkim@illinois.edu>
2020-03-28 22:10:24 -0500

    Made port connections for dut in testbench

--- mp3/hvl/top.sv
+++ mp3/hvl/top.sv
@@ -20,7 +20,7 @@ source_tb tb(
 // This section not required until CP3
 
 assign rvfi.commit = 0; // Set high when a valid instruction is modifying regfile or PC
-assign rvfi.halt = 0;   // Set high when you detect an infinite loop
+//assign rvfi.halt = 0;   // Set high when you detect an infinite loop
 initial rvfi.order = 0;
 always @(posedge itf.clk iff rvfi.commit) rvfi.order <= rvfi.order + 1; // Modify for OoO
 /**************************** End RVFIMON signals ****************************/
@@ -29,11 +29,30 @@ always @(posedge itf.clk iff rvfi.commit) rvfi.order <= rvfi.order + 1; // Modif
 // This section not required until CP2
 /*********************** End Shadow Memory Assignments ***********************/
 
-// Set this to the proper value
-assign itf.registers = '{default: '0};
+
 
 /*********************** Instantiate your design here ************************/
-mp3 dut();
+mp3 dut(
+  .clk (itf.clk),
+  .rst (itf.rst),
+
+  .inst_read (itf.inst_read),
+  .inst_addr (itf.inst_addr),
+  .inst_resp (itf.inst_resp),
+  .inst_rdata (itf.inst_rdata),
+
+  .data_read (itf.data_read),
+  .data_write (itf.data_write),
+  .data_mbe (itf.data_mbe),
+  .data_addr (itf.data_addr),
+  .data_wdata (itf.data_wdata),
+  .data_resp (itf.data_resp),
+  .data_rdata (itf.data_rdata)
+);
+
+// Set this to the proper value
+assign itf.registers = dut.ID.regfile.data;
+//assign rvfi.halt = dut.instruction_fetch.pc_load & (dut.instruction_fetch.pc_ff == dut.instruction_fetch.pc_out);
 /***************************** End Instantiation *****************************/
 
 endmodule
