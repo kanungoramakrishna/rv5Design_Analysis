db3b1cada75ae78c2326191ffec4badc80fcca7a
Chris Coronado <ccoron7@illinois.edu>
2020-04-19 14:22:04 -0500

    Hazard unit, first pass

--- mp3/hdl/CPU/hazard_unit.sv
+++ mp3/hdl/CPU/hazard_unit.sv
@@ -2,9 +2,47 @@ import rv32i_types::*;
 
 module hazard_unit
 (
-	// TODO I/O
+	input  rv32i_word instr, 
+	input  rv32i_control_word ctrl,
+	output logic stall //when asserted, generate NOPS and stall ID/IF
 );
 
-// TODO implement
+rv32i_reg rs1;
+rv32i_reg rs2;
 
+
+always_comb
+begin
+	rs1 = 0;
+	rs2 = 0;
+	stall = 0;
+
+	case (ctrl.opcode)
+		op_lui,op_auipc,op_jal,op_jal:
+		begin
+			rs1 = 0;
+			rs2 = 0;
+		end
+
+		op_jalr,op_load,op_imm:
+		begin
+			rs1 = instr[19:15];
+			rs2 = 0;
+		end
+
+		default:
+		begin
+			rs1 = instr[19:15];
+			rs2 = instr[24:20];
+		end
+	endcase
+
+	if (ctrl.opcode == op_load && ctrl.rd != 0)
+	begin
+		if (ctrl.rd == rs1 || ctrl.rd == rs2)
+		begin
+			stall == 1;
+		end
+	end
+end
 endmodule
