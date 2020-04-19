37d61b8a2147c4e419d2154d5941639bd0b43122
Chris Coronado <ccoron7@illinois.edu>
2020-04-19 15:39:05 -0500

    memory forwarding unit, first pass

--- mp3/hdl/CPU/mem_forward_unit.sv
+++ mp3/hdl/CPU/mem_forward_unit.sv
@@ -2,9 +2,23 @@ import rv32i_types::*;
 
 module mem_forward_unit
 (
-	// TODO I/O
+	input  rv32i_control_word ex_mem,
+	input  rv32i_word ex_mem_address, 
+	input  rv32i_control_word mem_wb,
+	input  rv32i_word mem_wb_address, 
+	output logic fwd, 
 );
 
-// TODO implement
+always_comb
+begin
+	fwd = 0; 
 
+	if (ex_mem.opcode == op_store && mem_wb.opcode == op_load)
+	begin
+		if (ex_mem_address == mem_wb_address)
+		begin
+			fwd = 1; 
+		end
+	end
+end
 endmodule
