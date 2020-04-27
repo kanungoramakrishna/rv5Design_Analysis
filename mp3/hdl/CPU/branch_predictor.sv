968b41d5b2db0717b57e4371f432e567cf49f0ec
Chris <coronadocx@gmail.com>
2020-04-27 18:59:05 -0500

    WIP

--- mp3/hdl/CPU/branch_predictor.sv
+++ mp3/hdl/CPU/branch_predictor.sv
@@ -1,10 +1,36 @@
 import rv32i_types::*;
 
-module branch_predictor
+module branch_predictor # (
+	parameter bht_size = 4
+)
 (
-	// TODO
+	input logic clk, rst, 
+	input rv32i_word instr,
+	input rv32i_word pc,
+	output rv32i_word addr, 
 );
 
-// TODO
+logic [1:0] branch_history_table [bht_size];
+logic [1:0] cur; 
+
+always_ff @(posedge clk)
+begin
+	if (rst)
+	begin
+		for (int i=0; i<bht_size; i++) begin
+			branch_history_table[i] <= 0;
+		end
+	end
+	else if (/*TODO*/)
+	begin
+		// Access table
+	end
+end
+
+always_comb
+begin
+	cur = branch_history_table[]
+
+end
 
 endmodule
