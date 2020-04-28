37655c4f5a47de547091aa413caeb43c7d6a1f99
Chris <coronadocx@gmail.com>
2020-04-27 19:47:24 -0500

    WIP

--- mp3/hdl/CPU/branch_predictor.sv
+++ mp3/hdl/CPU/branch_predictor.sv
@@ -1,24 +1,45 @@
 import rv32i_types::*;
 
-module branch_predictor # (
-	parameter bht_size = 4
-)
+module branch_predictor # (parameter idx_size)
 (
+	// ID I/O
 	input logic clk, rst, 
 	input rv32i_word instr,
 	input rv32i_word pc,
 	output rv32i_word addr, 
+
+	// EX I/O
+	input 
 );
 
-logic [1:0] branch_history_table [bht_size];
-logic [1:0] cur; 
+localparam bht_size = 2**idx_size; 
 
+logic [1:0] branch_history_table [bht_size];
+rv32i_word  branch_target_buffer [bht_size];
+logic       branch_target_valid  [bht_size]; 
+logic [idx_size-1:0] idx;
+
+logic [1:0] prediction;
+rv32i_word  target;
+logic       target_val;  
+
+// Access info during ID
+always_comb
+begin
+	idx = pc[(idx_size+1):2];
+	prediction = branch_history_table[idx];
+	target     = branch_target_buffer[idx];
+	target_val = branch_target_valid[idx]; 
+end
+// Update counters during EX
 always_ff @(posedge clk)
 begin
 	if (rst)
 	begin
 		for (int i=0; i<bht_size; i++) begin
 			branch_history_table[i] <= 0;
+			branch_target_buffer[i] <= 0;
+			branch_target_valid [i] <= 0;  
 		end
 	end
 	else if (/*TODO*/)
@@ -27,10 +48,5 @@ begin
 	end
 end
 
-always_comb
-begin
-	cur = branch_history_table[]
-
-end
 
 endmodule
